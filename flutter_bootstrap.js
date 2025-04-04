50e395590262e0bc8d2a2c704efd9ff9f2f9fcdb
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2025-04-04 12:17:48 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "675913333"
+    serviceWorkerVersion: "1730446171"
   }
 });
